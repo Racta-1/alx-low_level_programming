9dce8fc4cf6b578b1fb0a068e66f50b75c65b5ae
Racta-1 <tayeakinrele@gmail.com>
2023-04-21 06:55:01 -0700

    changes

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,11 +4,12 @@
  * main - print all possible combination of single digit number
  * i.e 0 - 9 followed by space and comma using ASCII code
  *
- * Always: Success (0)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
+
 	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
