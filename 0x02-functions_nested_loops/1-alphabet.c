c0f0422bd7a5be80b5e041e5e5f187029c0dad97
Racta-1 <tayeakinrele@gmail.com>
2023-04-25 07:07:06 -0700

    changes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
+
 /**
- * print_alphabet - Prints the alphabet in lowercase followed by a new line
+ * print_alphabet - Prints the alphabet in lowercase.
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	int i;
-	
+
 	for (i = 97; i <= 122; i++)
 	{
 		putchar(i);
