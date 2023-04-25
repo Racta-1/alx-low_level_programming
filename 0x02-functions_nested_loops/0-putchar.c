d00351a4842b3fae82792e2ee69f744fe1947edc
Racta-1 <tayeakinrele@gmail.com>
2023-04-25 01:10:34 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints "_putchar" as a message.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int list[] = {95, 112, 117, 116, 99, 104, 97, 114};
+
+	int count, size;
+
+	size = sizeof(list) / sizeof(list[0]);
+
+	for(count = 0; count < size; count++)
+	{
+		_putchar(list[count]);
+	}
+	_putchar('\n');
+	return (0);
+}
