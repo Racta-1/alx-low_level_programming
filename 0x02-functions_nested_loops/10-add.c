40055ca821bd9eeab7158d50534ce99d6c44f8e9
Racta-1 <tayeakinrele@gmail.com>
2023-04-25 06:20:03 -0700

    adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * add - add tow integers
+ *
+ * Return: result of the 
+ * sum of two numbers
+ */
+int add(int a, int b)
+{
+	int sum;
+	sum = a + b;
+	return (sum)
+}
