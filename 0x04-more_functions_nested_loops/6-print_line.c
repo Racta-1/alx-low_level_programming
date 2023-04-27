bdc7793a7a98da87911d180cdc999a58aa77bdfa
Racta-1 <tayeakinrele@gmail.com>
2023-04-27 11:27:53 -0700

    function that draws a straiht line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line in the terminal
+ * @n: number of dashes to add
+ *
+ * Return: result
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+			_putchar('\n');
+		_putchar(95);
+	}
+	_putchar('\n');
+}
