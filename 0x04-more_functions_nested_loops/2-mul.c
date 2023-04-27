b7fd838fb2624ccf0503130305480d2efde031e3
Racta-1 <tayeakinrele@gmail.com>
2023-04-27 10:17:21 -0700

    created multiply file that multiplies two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - a function that multiplies two integers
+ * @a: input integer number
+ * @b: input inter number
+ *
+ * Return: return the result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
